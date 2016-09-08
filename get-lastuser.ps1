bf00d3f6c117f9c0d8556d9030ad3ee8c4568c6c
Jeff Buenting <jeffbuenting@users.noreply.github.com>
2016-09-07 21:27:19 -0400

    adding

---
+++ get-lastuser.ps1
@@ -0,0 +1,25 @@
+﻿Function Get-LastUser( $Computer )
+
+{
+   dir "\\$Computer\c$\documents and settings" ntuser.dat -rec -force | sort lastwritetime -desc | select lastwritetime,directory 
+}
+ 
+#-----------------------------------------------------------------------------------------------------------------------------------------------
+
+Get-VMMServer "vbas0053" | Out-Null
+
+get-vm | where { ( $_.status -eq "Running" ) -and ( $_.hostname -eq "vbvs0002.vbgov.com" ) }| foreach { 
+
+	$length = 0
+    $length = ( $_.name ).indexof( " " ) 
+    if ( $length -le 0 ) { $length = ($_.name).length }
+    $strComputer = ( $_.name ).substring(0,$length)
+  
+   
+	get-lastuser $strComputer
+	
+}
+
+
+
+
