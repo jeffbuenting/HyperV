054a410ad7d4657849812177c07c17690f8aeea4
jeffbuenting <jeffbuenting@gmail.com>
2017-01-24 09:27:49 -0500

    Get-HVCPUPerformance
         Adding Pester test
         Adding support for runspaces (not complete yet)

---
+++ HV Dashboard/Get-HVCPUPerformance.Tests.ps1
@@ -0,0 +1,57 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut" | out-Null
+
+$TestCases = @(
+    @{
+        TestName = 'One Server'
+        ComputerName = $env:COMPUTERNAME
+    }
+    
+    @{
+        TestName = 'Multiple Servers'
+        ComputerName = $env:COMPUTERNAME,$env:COMPUTERNAME,$env:COMPUTERNAME
+    }
+
+    @{
+        TestName = 'No Server Name passed'  
+    }
+)
+
+Describe "Get-HVCPUPerformance" {
+    It "Returns a custom object.  Test: <TestName>" -TestCases $TestCases {
+        param 
+        (
+            $ComputerName
+        )
+
+        if ( $Computername ) 
+        {
+            $output = Get-HVCPUPerformance -ComputerName $ComputerName -Verbose
+        }
+        Else 
+        {
+            $output = Get-HVCPUPerformance -Verbose
+        }
+
+        $Output | Should BeofType PSCustomObject
+    }
+
+    It "Should be for counter Hyper-V HyperVisor Locical Processor % Total Run Time. Test: <TestName>" -TestCases $TestCases {
+        param 
+        (
+            $ComputerName
+        )
+
+        if ( $Computername ) 
+        {
+            $output = Get-HVCPUPerformance -ComputerName $ComputerName -Verbose
+        }
+        Else 
+        {
+            $output = Get-HVCPUPerformance -Verbose
+        }
+
+        $Output.Path | Should Match "hyper-v hypervisor logical processor\(.*\)\\% total run time"
+    }
+}
--- HV Dashboard/Get-HVCPUPerformance.ps1
+++ HV Dashboard/Get-HVCPUPerformance.ps1
@@ -3,17 +3,29 @@
     [CmdletBinding()]
     Param (
         [Parameter( ValueFromPipeline=$True )]
-        [String[]]$ComputerName
+        [String[]]$ComputerName = $Env:COMPUTERNAME,
+
+        [int]$MaxRunSpaces = $Env:NUMBER_OF_PROCESSORS + 1
     )
+
+    Begin
+    {
+        Write-verbose "Create Runspace Pool"
+        $RunSpacePool = [RunspaceFactory]::CreateRunspacePool( 1,$MaxRunSpaces)
+        $RunSpacePool.ApartmentState = 'MTA'
+        $RunSpacePool.Open()
+    }
 
     Process {
         ForEach ( $C in $ComputerName ) {
             Write-Verbose "Retrieving Perfromance data for $C"
 
             $CPURunTime = get-counter  -ComputerName $C -Counter "Hyper-V Hypervisor Logical Processor(*)\% Total Run Time" 
+
             $TimeStamp = $CPURunTime.TimeStamp
             $CPURunTime.CounterSamples | Foreach {
                 
+                # ----- Determine the health of the value
                 if ( $_.CookedValue -ge 90 ) { $H = 2 }
                     Elseif ( $_.CookedValue -ge 80 -and $_.CookedValue -Lt 90 ) { $H = 1 }
                         Else { $H = 0 }
@@ -25,12 +37,23 @@
                     PercentRunTime = $_.CookedValue
                     Health = $H
                 }
+
                 Write-Output $CPU
             }
         }
     }
+
+    End
+    {
+        Write-Verbose "Runspace cleanup"
+        $RunSpacePool.Close()
+        $RunSpacePool.Dispose()
+    }
 }
 
+$StopWatch = [system.diagnostics.stopwatch]::StartNew()
 
 
-$CPU = 'SL-Jeffb' | Get-HVCPUPerformance -Verbose
+ 'SL-Jeffb' | Get-HVCPUPerformance -Verbose
+
+ $StopWatch.Elapsed
