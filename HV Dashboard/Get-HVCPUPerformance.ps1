517da3113a8211807be59babc0434d6c9b3b5c5f
jeffbuenting <jeffbuenting@gmail.com>
2017-05-23 10:01:08 -0400

    runspace update

--- HV Dashboard/Get-HVCPUPerformance.ps1
+++ HV Dashboard/Get-HVCPUPerformance.ps1
@@ -14,13 +14,13 @@
         $RunSpacePool = [RunspaceFactory]::CreateRunspacePool( 1,$MaxRunSpaces)
         $RunSpacePool.ApartmentState = 'MTA'
         $RunSpacePool.Open()
-    }
 
-    Process {
-        ForEach ( $C in $ComputerName ) {
-            Write-Verbose "Retrieving Perfromance data for $C"
-
-            $CPURunTime = get-counter  -ComputerName $C -Counter "Hyper-V Hypervisor Logical Processor(*)\% Total Run Time" 
+        $Command = {
+            param (
+                [String]$Computer
+            )
+            
+            $CPURunTime = get-counter -ComputerName $Computer -Counter "Hyper-V Hypervisor Logical Processor(*)\% Total Run Time" 
 
             $TimeStamp = $CPURunTime.TimeStamp
             $CPURunTime.CounterSamples | Foreach {
@@ -31,7 +31,7 @@
                         Else { $H = 0 }
                 
                 $CPU = New-Object psobject -Property @{
-                    ComputerName = $C
+                    ComputerName = $Computer
                     Path = $_.Path
                     InstanceName = $_.InstanceName
                     PercentRunTime = $_.CookedValue
@@ -40,6 +40,31 @@
 
                 Write-Output $CPU
             }
+        }
+    }
+
+    Process {
+        ForEach ( $C in $ComputerName ) {
+            Write-Verbose "Retrieving Perfromance data for $C"
+
+            $RunSpaceObject = New-Object -TypeName PSObject -Property @{
+                Runspace = [Powershell]::Create()
+                Invoker = $Null
+            }
+
+            $RunSpaceObject.RunSpace.RunspacePool = $RunSpacePool
+            $RunSpaceObject.Runspace.AddScript( $Command ) | Out-Null
+            $runSpaceObject.RunSpace.AddArgument( $C ) | Out-Null
+            $RunSpaceObject.Invoker = $RunspaceObject.RunSpace.BeginInvoke()
+
+            # ----- Wait for Runspace to complete
+            Write-Verbose "Waiting for Runspace to complete for $C"
+            While ( $RunSpaceObject.Invoker.IsCompleted -eq $False ) {}
+
+            $HVCPU = $RunSpaceObject.Runspace.EndInvoke( $RunSpaceObject.Invoker )
+            $RunspaceObject.Runspace.Dispose()
+
+            Write-Output $HVCPU
         }
     }
 
@@ -54,6 +79,6 @@
 $StopWatch = [system.diagnostics.stopwatch]::StartNew()
 
 
- 'SL-Jeffb' | Get-HVCPUPerformance -Verbose
+ 'SL-Hyperv1.stratuslivedemo.com','SL-Jeffb' | Get-HVCPUPerformance -Verbose
 
  $StopWatch.Elapsed
---
+++ HV Dashboard/Get-HVCPURunspace.ps1
@@ -0,0 +1,39 @@
+﻿Function Get-HVCPU {
+
+    [CmdletBinding()]
+    Param (
+        [Parameter( ValueFromPipeline=$True )]
+        [String[]]$ComputerName
+    )
+
+    Process {
+        ForEach ( $C in $ComputerName ) {
+            Write-Verbose "Retrieving Perfromance data for $C"
+
+            $CPURunTime = get-counter  -ComputerName $C -Counter "Hyper-V Hypervisor Logical Processor(*)\% Total Run Time" 
+            $TimeStamp = $CPURunTime.TimeStamp
+            $CPURunTime.CounterSamples | Foreach {
+                
+                if ( $_.CookedValue -ge 90 ) { $H = 2 }
+                    Elseif ( $_.CookedValue -ge 80 -and $_.CookedValue -Lt 90 ) { $H = 1 }
+                        Else { $H = 0 }
+                
+                $CPU = New-Object psobject -Property @{
+                    ComputerName = $C
+                    Path = $_.Path
+                    InstanceName = $_.InstanceName
+                    PercentRunTime = $_.CookedValue
+                    Health = $H
+                }
+                Write-Output $CPU
+            }
+        }
+    }
+}
+
+$StopWatch = [system.diagnostics.stopwatch]::StartNew()
+
+
+ 'SL-Hyperv1.stratuslivedemo.com','SL-Jeffb' | Get-HVCPu -Verbose
+
+ $StopWatch.Elapsed
